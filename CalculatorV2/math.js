12dc00f08f5179d2dd728a8a50f04001f316576e
yoonseooo71 <yoonseooo71@gmail.com>
2022-10-14 18:49:47 +0900

    22.10.14

--- CalculatorV2/math.js
+++ CalculatorV2/math.js
@@ -1,4 +1,4 @@
 const numBtns = document.querySelectorAll(".num-btn"); 
-for (let i=0; i <numBtns.length;i++) {
-  numBtns[i].addEventListener("click",()=>{console.log(`${numBtns[i].innerText}`);})
+for (let numBtn of numBtns) {
+  numBtn.addEventListener("click",()=>{console.log(`${numBtn.innerText}`);})
 }
